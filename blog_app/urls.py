cc1908656f72c0df1a0ac6a2a2c649205d2d54cb
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-05-02 20:13:42 +0545

    asjhbahjs

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="blog-home"),
-    path("about/", views.about, name="blog-about")
+    path("", views.Home.as_view(), name="blog-home"),
+    path("about/", views.about, name="blog-about"),
+    path("post/<int:pk>/", views.PostDetail.as_view(), name="post-detail")
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import render
 from .models import BlogPostModel
+from django.contrib.auth.decorators import login_required
+
+from django.views.generic import ListView, DetailView
 
 
-def home(request):
-    context = {
-        'posts': BlogPostModel.objects.all()
-    }
-    return render(request, "blog/home.html", context)
+# def home(request):
+#     context = {
+#         'posts': BlogPostModel.objects.all()
+#     }
+#     return render(request, "blog/home.html", context)
 
 
 def about(request):
     return render(request, "blog/about.html")
+
+# @login_required()
+# def create_post(request):
+#     if request.method == "POST":
+
+
+class Home(ListView):
+    model = BlogPostModel
+    template_name = "blog_app/home.html"
+    context_object_name = "posts"
+    ordering = ['-posted_date']
+
+
+class PostDetail(DetailView):
+    model = BlogPostModel
+    context_object_name = "post"
