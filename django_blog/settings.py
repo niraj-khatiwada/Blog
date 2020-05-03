1d0374deea8ca455094200ab2aa46a6309e4c304
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-05-03 10:43:19 +0545

    Postgres

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -83,13 +83,8 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
-        'HOST': 'localhost',
-        'PORT': '5432'
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
