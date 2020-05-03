9c2f1edb44d944dde98c88a1d65dd8cd2f76b60c
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-05-03 14:06:49 +0545

    Debug

--- accounts/forms.py
+++ accounts/forms.py
@@ -16,4 +16,4 @@ class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ["first_name", "last_name",
-                  "description", "profile_image"]
+                  "description"]
